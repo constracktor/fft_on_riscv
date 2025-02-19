82d17f8085e77c6211eecaf252a78776bd66e6a9
Alexander Strack <alex@rostam1.rostam.cct.lsu.edu>
2025-02-19 16:45:12 -0600

    Adjust to sven

--- compile.sh
+++ compile.sh
@@ -15,25 +15,31 @@ then
     export HPX_DIR="$ROOT/hpx_scripts/hpx_1.9_mpi/install/lib"
     # MPI compiler
     export CXX="/usr/local.nfs/sgs/software/ipvs-epyc/openmpi/4.0.4-gcc10.2/bin/mpic++"
+    # FFTW paths
+    export FFTW_TH_DIR="$ROOT/fftw_scripts/fftw_threads_mpi/install/lib"
+    export FFTW_OMP_DIR="$ROOT/fftw_scripts/fftw_omp_mpi/install/lib"
+    export FFTW_HPX_DIR="$ROOT/fftw_scripts/fftw_hpx/install/lib"
+    export PKG_CONFIG_PATH="$FFTW_TH_DIR/pkgconfig":$PKG_CONFIG_PATH
 elif [[ "$1" == "sven" ]]
 then
     # sven
+    module load openmpi/5.0.3
     export FFTW_DIR=sven
     # HPX lib directory
-    export HPX_DIR="${HOME}/hpxsc_installations/hpx_1.9.1_gcc_13.2.1_sven/build/hpx/lib64"
+    export HPX_DIR="${HOME}/hpxsc_installations/hpx_1.10.0_gcc_13.2.1_sven/build/lib"
     # MPI compiler
-    module load mpi/openmpi-riscv64
-    export CXX=mpic++
+    export CXX=/opt/apps/openmpi/5.0.3/bin/mpic++
+    # FFTW paths
+    export FFTW_TH_DIR="$HOME/fft_installations/$FFTW_DIR/fftw_threads_mpi/install/lib"
+    export FFTW_OMP_DIR="$HOME/fft_installations/$FFTW_DIR/fftw_omp_mpi/install/lib"
+    export FFTW_HPX_DIR="$HOME/fft_installations/$FFTW_DIR/fftw_hpx/install/lib"
+    # for sven with measure planning: export FFTW_HPX_DIR="$HOME/fft_installations/$FFTW_DIR/fftw_hpx_riscv/install/lib"
+    export PKG_CONFIG_PATH="$FFTW_TH_DIR/pkgconfig":$PKG_CONFIG_PATH
 else
-  echo 'Please specify system: "epyc" or "buran_mpi" or "buran_lci"'
+  echo 'Please specify system: "epyc" or "sven"'
   exit 1
 fi
 export CMAKE_COMMAND=cmake
-# FFTW paths
-export FFTW_TH_DIR="$ROOT/fftw_scripts/fftw_threads_mpi/install/lib"
-export FFTW_OMP_DIR="$ROOT/fftw_scripts/fftw_omp_mpi/install/lib"
-export FFTW_HPX_DIR="$ROOT/fftw_scripts/fftw_hpx/install/lib"
-export PKG_CONFIG_PATH="$FFTW_TH_DIR/pkgconfig":$PKG_CONFIG_PATH
 
 ################################################################################
 # Compilation
