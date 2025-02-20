6f5ed377b16cfafb7474a199647beba4b9206860
Alexander Strack <alex@rostam1.rostam.cct.lsu.edu>
2025-02-20 13:41:12 -0600

    Adjust compile script to local riscv fft

--- compile.sh
+++ compile.sh
@@ -12,38 +12,34 @@ then
     module load cmake
     module load ipvs-epyc/openmpi/4.0.4-gcc-10.2
     spack load hpx
+    # HPX lib directory
     export HPX_DIR="$ROOT/hpx_scripts/hpx_1.9_mpi/install/lib"
     # MPI compiler
     export CXX="/usr/local.nfs/sgs/software/ipvs-epyc/openmpi/4.0.4-gcc10.2/bin/mpic++"
-    # FFTW paths
-    export FFTW_TH_DIR="$ROOT/fftw_scripts/fftw_threads_mpi/install/lib"
-    export FFTW_OMP_DIR="$ROOT/fftw_scripts/fftw_omp_mpi/install/lib"
-    export FFTW_HPX_DIR="$ROOT/fftw_scripts/fftw_hpx/install/lib"
-    export PKG_CONFIG_PATH="$FFTW_TH_DIR/pkgconfig":$PKG_CONFIG_PATH
-elif [[ "$1" == "sven" ]]
+
+elif [[ "$1" == "riscv" ]]
 then
     # sven
     module load openmpi/5.0.3
-    export FFTW_DIR=sven
     # HPX lib directory
-    export HPX_DIR="${HOME}/hpxsc_installations/hpx_1.10.0_gcc_13.2.1_sven/build/lib"
+    export HPX_DIR="${ROOT}/hpx_scripts/hpx_1.9_riscv/install/lib"
     # MPI compiler
     export CXX=/opt/apps/openmpi/5.0.3/bin/mpic++
-    # FFTW paths
-    export FFTW_TH_DIR="$HOME/fft_installations/$FFTW_DIR/fftw_threads_mpi/install/lib"
-    export FFTW_OMP_DIR="$HOME/fft_installations/$FFTW_DIR/fftw_omp_mpi/install/lib"
-    export FFTW_HPX_DIR="$HOME/fft_installations/$FFTW_DIR/fftw_hpx/install/lib"
-    # for sven with measure planning: export FFTW_HPX_DIR="$HOME/fft_installations/$FFTW_DIR/fftw_hpx_riscv/install/lib"
-    export PKG_CONFIG_PATH="$FFTW_TH_DIR/pkgconfig":$PKG_CONFIG_PATH
 else
   echo 'Please specify system: "epyc" or "sven"'
   exit 1
 fi
-export CMAKE_COMMAND=cmake
+
+# FFTW paths
+export FFTW_TH_DIR="$ROOT/fftw_scripts/$1/fftw_threads_mpi/install/lib"
+export FFTW_OMP_DIR="$ROOT/fftw_scripts/$1/fftw_omp_mpi/install/lib"
+export FFTW_HPX_DIR="$ROOT/fftw_scripts/$1/fftw_hpx/install/lib"
+export PKG_CONFIG_PATH="$FFTW_TH_DIR/pkgconfig":$PKG_CONFIG_PATH
+# for sven with measure planning: export FFTW_HPX_DIR="$HOME/fft_installations/$FFTW_DIR/fftw_hpx_riscv/install/lib"
 
 ################################################################################
 # Compilation
 ################################################################################
 rm -rf $BUILD_DIR && mkdir $BUILD_DIR && cd $BUILD_DIR
-$CMAKE_COMMAND .. -DCMAKE_BUILD_TYPE=Release -DHPX_DIR="${HPX_DIR}/cmake/HPX"
+cmake .. -DCMAKE_BUILD_TYPE=Release -DHPX_DIR="${HPX_DIR}/cmake/HPX"
 make -j $(grep -c ^processor /proc/cpuinfo)
