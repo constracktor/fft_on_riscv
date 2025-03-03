bd2a376067dec806a3279ed817c3aba5e8577a8e
Alexander Strack <alex@rostam1.rostam.cct.lsu.edu>
2025-03-03 03:14:53 -0600

    Add scripts for new fftw riscv release

--- fftw_scripts/riscv/install_fftw_omp_mpi_riscv.sh
+++ fftw_scripts/riscv/install_fftw_omp_mpi_riscv.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# This script installs FFTW3 from the recommended tarball with MPI+pthreads backend
+# This script installs FFTW3 from the recommended tarball with MPI+OpenMP backend
 #
 # The folling paths have to be exported:
 # $MPI_DIR: path to MPI installation (or load MPI module)
@@ -9,7 +9,7 @@ module load gcc/13.2.1
 module load openmpi/5.0.3
 #export PATH="$MPI_DIR/bin":$PATH
 # structure
-export ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd )/fftw_threads_mpi_riscv"
+export ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd )/fftw_omp_mpi_riscv"
 export DIR_SRC="$ROOT/src"
 export DIR_BUILD="$ROOT/build"
 export DIR_INSTALL="$ROOT/install"
@@ -21,7 +21,7 @@ wget -O- ${DOWNLOAD_URL} | tar xz --strip-components=1
 # build
 mkdir -p $DIR_BUILD
 cd $DIR_BUILD
-$DIR_SRC/configure --prefix=$DIR_INSTALL --enable-mpi --enable-threads
+$DIR_SRC/configure --prefix=$DIR_INSTALL --enable-mpi --enable-openmp
 make -j $(grep -c ^processor /proc/cpuinfo)
 # install
 make install
