53200a665ed9fdd4df9ef357fa805fcdd0133f5b
constracktor <strack.as@icloud.com>
2025-03-06 14:38:38 +0100

    MWE branch initial commit

--- compile.sh
+++ compile.sh
@@ -6,46 +6,19 @@
 export ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd )"
 # export paths
 export BUILD_DIR=build_$1
-if [[ "$1" == "epyc" ]]
-then
     # epyc
     module load cmake
     module load gcc/13.3.0
-    #spack load boost hwloc
-    #spack load hpx
     spack load openmpi@5.0.3%gcc@13.3.0
-    # HPX lib directory
-    export HPX_DIR="$ROOT/hpx_scripts/hpx_1.10_mpi/install/lib"
-    #export LD_LIBRARY_PATH=$HPX_DIR:$LD_LIBRARY_PATH
     # MPI compiler
     export CXX="mpic++"
-elif [[ "$1" == "riscv" ]]
-then
-    # riscv
-    module laoad gcc/13.2.1
-    module load openmpi/5.0.3
-    # HPX lib directory
-    export HPX_DIR="${ROOT}/hpx_scripts/hpx_1.10_riscv/install/lib"
-    # MPI compiler
-    export CXX=mpic++
-    #export CXX=/opt/apps/openmpi/5.0.3/bin/mpic++
-    # FFTW paths
-    FFTW_RISCV="_riscv"
-else
-  echo 'Please specify system: "epyc" or "riscv"'
-  exit 1
-fi
-
 # FFTW paths
-#export FFTW_TH_DIR="$ROOT/fftw_scripts/$1/fftw_threads_mpi$FFTW_RISCV/install/lib"
 export FFTW_OMP_DIR="$ROOT/fftw_scripts/$1/fftw_omp_mpi$FFTW_RISCV/install/lib"
-export FFTW_HPX_DIR="$ROOT/fftw_scripts/$1/fftw_hpx/install/lib"
 export PKG_CONFIG_PATH="$FFTW_OMP_DIR/pkgconfig":$PKG_CONFIG_PATH
-# for riscv with measure planning: export FFTW_HPX_DIR="$HOME/fft_installations/$FFTW_DIR/fftw_hpx_riscv/install/lib"
 
 ################################################################################
 # Compilation
 ################################################################################
 rm -rf $BUILD_DIR && mkdir $BUILD_DIR && cd $BUILD_DIR
-cmake .. -DCMAKE_BUILD_TYPE=Release -DHPX_DIR="${HPX_DIR}/cmake/HPX"
+cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j $(grep -c ^processor /proc/cpuinfo)
--- shared_benchmark.sh
+++ shared_benchmark.sh
@@ -1,40 +1,22 @@
 #!/bin/bash
 ################################################################################
 # Benchmark script for shared memory 
-# $1: FFTW planning flag (estimate/measure)
-# $2: partition (epyc/sven)
-if [[ "$2" == "epyc" ]]
-then
-    PARTITION=epyc
-    THREAD_POW=6
-    BUILD_DIR=build_epyc
-elif [[ "$2" == "riscv" ]]
-then
-    PARTITION="risc5 -w sven0"
-    THREAD_POW=6
-    BUILD_DIR=build_riscv
-else
-  echo 'Please specify partition'
-  exit 1
-fi
+PARTITION=epyc
+THREAD_POW=6
+BUILD_DIR=build_epyc
 LOOP=1
 #0
-FFTW_PLAN=$1
+FFTW_PLAN=estimate
 THREADS=$((2**$THREAD_POW))
 cd benchmark
-# HPX implementations
-# shared only
-#sbatch -p $PARTITION -N 1 -n 1 -c $THREADS run_hpx_shared.sh $BUILD_DIR/fft_hpx_loop_shared $FFTW_PLAN $THREAD_POW $LOOP
-# sbatch -p $PARTITION -N 1 -n 1 -c $THREADS run_hpx_shared.sh $BUILD_DIR/fft_hpx_task_sync_shared $FFTW_PLAN $THREAD_POW $LOOP
-# sbatch -p $PARTITION -N 1 -n 1 -c $THREADS run_hpx_shared.sh $BUILD_DIR/fft_hpx_task_shared $FFTW_PLAN $THREAD_POW $LOOP
-# sbatch -p $PARTITION -N 1 -n 1 -c $THREADS run_hpx_shared.sh $BUILD_DIR/fft_hpx_task_naive_shared $FFTW_PLAN $THREAD_POW $LOOP
-# FFTW backends
-# shared only
-# sbatch -p $PARTITION -N 1 -n 1 -c $THREADS run_fftw_shared.sh $BUILD_DIR/fftw_hpx $FFTW_PLAN $THREAD_POW $LOOP
-# sbatch -p $PARTITION -N 1 -n 1 -c $THREADS run_fftw_shared.sh $BUILD_DIR/fftw_threads $FFTW_PLAN $THREAD_POW $LOOP
-#sbatch -p $PARTITION -N 1 -n 1 -c $THREADS run_fftw_shared.sh $BUILD_DIR/fftw_omp $FFTW_PLAN $THREAD_POW $LOOP
-# # distributed possible
-# sbatch -p $PARTITION -N 1 -n 1 -c $THREADS run_fftw_shared.sh $BUILD_DIR/fftw_mpi_threads $FFTW_PLAN $THREAD_POW $LOOP
-#sbatch -p $PARTITION -N 1 -n 1 -c $THREADS run_fftw_shared.sh $BUILD_DIR/fftw_mpi_omp $FFTW_PLAN $THREAD_POW $LOOP
-# MPI
-sbatch -p $PARTITION -N 1 -n $THREADS -c 1 ./run_fftw_mpi.sh $BUILD_DIR/fftw_mpi_omp $FFTW_PLAN $THREAD_POW $LOOP
+if [[ "$1" == "slurm" ]]
+then
+    # MPI
+    sbatch -p $PARTITION -N 1 -n $THREADS -c 1 ./run_fftw_mpi.sh $BUILD_DIR/fftw_mpi_omp $FFTW_PLAN $THREAD_POW $LOOP
+elif [[ "$1" == "node" ]]
+then
+    ./run_fftw_mpi_local.sh $BUILD_DIR/fftw_mpi_omp $FFTW_PLAN $THREAD_POW $LOOP
+else
+    echo 'Please specify node or slurm'
+  exit 1
+fi
