050641148f9e7f9c0bb70b3630f8da593a44d39a
Alexander Strack <alex@rostam1.rostam.cct.lsu.edu>
2025-06-02 08:58:34 -0500

    Separate fftw benchmark scripts

--- benchmark/run_fftw_omp.sh
+++ benchmark/run_fftw_omp.sh
@@ -18,7 +18,7 @@
 # $4: Number of runs
 # Parameters
 LOOP=$4
-POW_START=1
+POW_START=4
 POW_STOP=$3
 BASE_SIZE=16384
 # Get run command
@@ -31,17 +31,17 @@ pwd; hostname; date
 mkdir result
 mkdir plans
 # Strong scaling loop from 2^pow_start to 2^pow_stop cores
-FFTW3_HPX_NTHREADS=1 OMP_PROC_BIND=CLOSE OMP_PLACES=cores$COMMAND $EXECUTABLE 1 $ARGUMENTS 1 
-for (( j=1; j<$LOOP; j=j+1 ))
-do
-    FFTW3_HPX_NTHREADS=1 $COMMAND $EXECUTABLE 1 $ARGUMENTS 0
-done
+#OMP_PROC_BIND=CLOSE OMP_PLACES=cores $COMMAND $EXECUTABLE 1 $ARGUMENTS 1 
+#for (( j=1; j<$LOOP; j=j+1 ))
+#do
+#    $COMMAND $EXECUTABLE 1 $ARGUMENTS 0
+#done
 for (( i=2**$POW_START; i<=2**$POW_STOP; i=i*2 ))
 do
     COMMAND="srun --cpu-bind=cores -N 1 -n 1 -c $i"
     for (( j=0; j<$LOOP; j=j+1 ))
     do
-        FFTW3_HPX_NTHREADS=$i OMP_PROC_BIND=CLOSE OMP_PLACES=cores $COMMAND $EXECUTABLE $i $ARGUMENTS 0
+        OMP_PROC_BIND=CLOSE OMP_PLACES=cores $COMMAND $EXECUTABLE $i $ARGUMENTS 0
     done
 done
 # Log Info
