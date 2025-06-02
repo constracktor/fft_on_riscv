a6e9f8e53941826819c0888bb704b990089ecabf
Alexander Strack <alex@rostam1.rostam.cct.lsu.edu>
2025-06-02 08:38:58 -0500

    Adjust riscv compilation

--- compile.sh
+++ compile.sh
@@ -6,9 +6,9 @@
 export ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd )"
 # export paths
 export BUILD_DIR=build_$1
-if [[ "$1" == "epyc" ]]
+if [[ "$1" == "x86" ]]
 then
-    # epyc
+    # x86
     module load cmake
     module load gcc/13.3.0
     #spack load boost hwloc
@@ -25,23 +25,19 @@ then
     module laoad gcc/13.2.1
     module load openmpi/5.0.3
     # HPX lib directory
-    export HPX_DIR="${ROOT}/hpx_scripts/hpx_1.10_riscv/install/lib"
+    export HPX_DIR="/home/alex/git_workspace/fft_on_riscv/hpx_scripts/build-scripts/build/hpx/lib64"
     # MPI compiler
     export CXX=mpic++
-    #export CXX=/opt/apps/openmpi/5.0.3/bin/mpic++
     # FFTW paths
     FFTW_RISCV="_riscv"
 else
-  echo 'Please specify system: "epyc" or "riscv"'
+  echo 'Please specify system: "x86" or "riscv"'
   exit 1
 fi
 
 # FFTW paths
-#export FFTW_TH_DIR="$ROOT/fftw_scripts/$1/fftw_threads_mpi$FFTW_RISCV/install/lib"
 export FFTW_OMP_DIR="$ROOT/fftw_scripts/$1/fftw_omp_mpi$FFTW_RISCV/install/lib"
-export FFTW_HPX_DIR="$ROOT/fftw_scripts/$1/fftw_hpx/install/lib"
 export PKG_CONFIG_PATH="$FFTW_OMP_DIR/pkgconfig":$PKG_CONFIG_PATH
-# for riscv with measure planning: export FFTW_HPX_DIR="$HOME/fft_installations/$FFTW_DIR/fftw_hpx_riscv/install/lib"
 
 ################################################################################
 # Compilation
